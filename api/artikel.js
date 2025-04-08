8b7c693cb22edd1edad49dfb320e13f7d4fdd2ac
LennartHemme <lennart.hemme@radioemscherlippe.de>
2025-04-08 14:19:06 +0200

    fetch neu

--- api/artikel.js
+++ api/artikel.js
@@ -1,3 +1,4 @@
+import fetch from 'node-fetch';
 export default async function handler(req, res) {
   const url = "https://script.google.com/macros/s/AKfycbx3hfePMrOb_XUArEGdG-_8XVqicJxMu5mmUyzNvcApEbnO2rgK0DQdFzWID9yugYGH/exec?action=getArtikelListe";
   try {
